b410ad04f5705638d79f384513b3e7d73051f6c7
Lucas Almeida <lucasags.dev@gmail.com>
2023-01-10 23:53:39 -0300

    JavaScript do Faq

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function animaisFantasticos() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-function activeTab(index) {
-  tabContent.forEach(section => {
-      section.classList.remove("ativo");
-  });
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-  tabContent[index].classList.add("ativo");
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", function () {
+        activeTab(index);
+      });
+    });
+  }
 }
+animaisFantasticos();
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener("click", function () {
-    activeTab(index);
-  });
-});
+function faq() {
+  const faqLista = document.querySelectorAll(".js-faq dt");
+  if (faqLista.length) {
+    faqLista[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion(event) {
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    faqLista.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+faq();
